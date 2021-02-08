df71fef143b812b164433b6b4c6c069f122ac6e0
Timo Wilken <timo.wilken@cern.ch>
2021-02-08 23:29:13 +0100

    Remove unused code and clean up formatting and logging
    - remove now-unused JGet
    - clean up imports
    - use built-in formatting of logging functions
    - clean up some formatting and unneeded list comprehensions

--- publish/run-all-rpms.sh
+++ publish/run-all-rpms.sh
@@ -5,7 +5,7 @@
 
 for CONF in aliPublish*-rpms.conf; do
   echo === $(LANG=C date) :: running for configuration $CONF === >&2
-  ./aliPublish --config "$CONF" --debug --cache-deps-dir /tmp/pubdepscache sync-rpms >&2
+  ./aliPublish --config "$CONF" --debug sync-rpms >&2
   timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/RPMS/el7.x86_64/ rpms3:alibuild-repo/RPMS/el7.x86_64/ || true
   timeout 300 rclone sync --config /secrets/alibuild_rclone_config --transfers=10 --verbose local:/repo/UpdRPMS/el7.x86_64/ rpms3:alibuild-repo/UpdRPMS/el7.x86_64/ || true
   printf "\n\n\n\n" >&2
